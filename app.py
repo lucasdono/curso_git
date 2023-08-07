febf5ed981ee332ac082d2e7af2965fd021b57b5
Lucas <l.d-1@hotmail.com>
2023-08-07 15:32:18 -0300

    testando merge

--- app.py
+++ app.py
@@ -53,7 +53,15 @@ def create_answers_section(df):
 
 
 def main():
-    df_raw = load_data()
-    create_dataframe_section(df_raw)
-    create_answers_section(df_raw)
+    df = load_data()
+
+    create_dataframe_section(df)
+
+    create_answers_section(df)
     
+    st.dataframe(df)
+
+
+
+if __name__ == '__main__':
+    main()
